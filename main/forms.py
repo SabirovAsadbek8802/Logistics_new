9532dde4dfe99a7abec1fd36779734e8f65bf7da
Quvonchbek Bobojonov <111079758+QuvonchbekBobojonov@users.noreply.github.com>
2024-05-16 12:15:02 +0500

    Update base.html

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator, EmailValidator
+from django.forms import CharField
+
 
 class ModalForm(forms.Form):
     email = forms.EmailField(validators=[EmailValidator(message="Invalid email address.")])
@@ -11,14 +13,47 @@ class ModalForm(forms.Form):
         )
     ])
 
-class ContactForm(forms.Form):
-    email = forms.EmailField()
-    phone_number = forms.CharField(max_length=20)
-    fullname = forms.CharField(max_length=255)
-    delivery = forms.CharField(max_length=255, label='Delivery ZIP code')
-    vehicle_type = forms.CharField(max_length=255)
-    pick_up = forms.CharField(max_length=255)
 
+class ContactForm(forms.Form):
+    car_types = [
+        ('Car', 'Car'),
+        ('SUV', 'SUV'),
+        ('Pickup', 'Pickup'),
+        ('Motorcycle', 'Motorcycle'),
+        ('Van', 'Van'),
+        ('ATV', 'ATV'),
+        ('RV', 'RV'),
+        ('Boat', 'Boat'),
+        ('Heavy', 'Heavy'),
+        ('Equipment', 'Equipment'),
+        ('Large', 'Large'),
+        ('Yacht', 'Yacht'),
+        ('Other', 'Other')
+    ]
+    car_run_choices = [
+        ('Open', 'Open'),
+        ('Driveway', 'Driveway'),
+        ('Enclosed', 'Enclosed'),
+    ]
+    email = forms.EmailField(label="Email address")
+    phone_number = forms.CharField(max_length=20, label="Phone number")
+    first_name = forms.CharField(max_length=255, label="First Name")
+    last_name = forms.CharField(max_length=255, label="Last Name")
+    car_type = forms.ChoiceField(label="Type", choices=car_types, widget=forms.Select(attrs={'class': 'nice-select'}))
+    car_year = forms.CharField(max_length=255, label="Year")
+    car_make = forms.CharField(max_length=255, label="Make")
+    car_model = forms.CharField(max_length=255, label="Model")
+    car_run = forms.ChoiceField(label="Running Condition", choices=car_run_choices,
+                                widget=forms.Select(attrs={'class': 'nice-select'}))
+    type_of_carrier = forms.CharField(max_length=255, label="Type of Carrier")
+    origin_city = forms.CharField(max_length=255, label="Origin City")
+    origin_state = forms.CharField(max_length=255, label="Origin State")
+    origin_zip = forms.CharField(max_length=255, label="Origin Zip")
+    destination_city = forms.CharField(max_length=255, label="Destination City")
+    destination_state = forms.CharField(max_length=255, label="Destination State")
+    destination_zip = forms.CharField(max_length=255, label="Destination Zip")
+    comment = forms.CharField(max_length=255, label="Comment", widget=forms.Textarea())
+    proposed_ship_date = forms.CharField(label="Proposed Ship Date")
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
@@ -29,16 +64,6 @@ class ContactForm(forms.Form):
         elif len(email) < 16:
             raise ValidationError("Email can not be less than 6 characters!")
         return email
-    
-    def clean_phone_number(self):
-        phone_number = self.cleaned_data.get('phone_number')
-        if not phone_number.startswith('+'):
-            raise ValidationError("Phone number must start with +")
-        if len(phone_number) > 32:
-            raise ValidationError("Phone number must be 32 characters or less!")
-        elif len(phone_number) < 6:
-            raise ValidationError("Phone number can not be less than 6 characters!")
-        return phone_number
 
 # class FooterForm(forms.Form):
 #     call_number = forms.CharField(max_length=20)   
--- main/views.py
+++ main/views.py
@@ -8,16 +8,17 @@ from django.core.mail import send_mail
 class HomeView(View):
     template_name = 'index.html'
     context = {
-            'form': ContactForm(),
-            'form_modal': ModalForm()
-            }
+        'form': ContactForm(),
+        'form_modal': ModalForm()
+    }
+
     def get(self, request):
         return render(request, self.template_name, context=self.context)
-    
+
     def post(self, request):
         email = request.POST.get('email', None)
         phone_number = request.POST.get('phone_number', None)
-        
+
         subject = 'Thank you for contacting us!'
         message = f'Hi {email},\n\nThank you for contacting us. We have received your inquiry and will get back to you shortly.'
         from_email = settings.EMAIL_HOST_USER
@@ -31,12 +32,15 @@ class HomeView(View):
         send_mail(admin_subject, admin_message, from_email, [admin_email])
 
         return redirect('home')
-        
+
+
 class AboutView(View):
     template_name = 'about.html'
+
     def get(self, request):
         return render(request, self.template_name)
-    
+
+
 class ContactView(View):
     template_name = 'contact.html'
     context = {'form': ContactForm()}
@@ -45,25 +49,58 @@ class ContactView(View):
         return render(request, self.template_name, context=self.context)
 
     def post(self, request):
-        fullname = request.POST.get('fullname', None)
         email = request.POST.get('email', None)
         phone_number = request.POST.get('phone_number', None)
-        pickup = request.POST.get('pickup', None)
-        delivery = request.POST.get('delivery', None)
-        vehicle_type = request.POST.get('vehicle_type', None)
-        transport_type = request.POST.get('transport_type', None)
-        car_info = request.POST.get('car_info', None)
+        first_name = request.POST.get('first_name', None)
+        last_name = request.POST.get('last_name', None)
+        car_type = request.POST.get('car_type', None)
+        car_year = request.POST.get('car_year', None)
+        car_make = request.POST.get('car_make', None)
+        car_model = request.POST.get('car_model', None)
+        car_run = request.POST.get('car_run', None)
+        type_of_carrier = request.POST.get('type_of_carrier', None)
+        origin_city = request.POST.get('origin_city', None)
+        origin_state = request.POST.get('origin_state', None)
+        origin_zip = request.POST.get('origin_zip', None)
+        destination_city = request.POST.get('destination_city', None)
+        destination_state = request.POST.get('destination_state', None)
+        destination_zip = request.POST.get('destination_zip', None)
+        comment = request.POST.get('comment', None)
+        proposed_ship_date = request.POST.get('proposed_ship_date', None)
 
+        # Create the subject and message for the user
+        fullname = f'{first_name} {last_name}'
         subject = 'Thank you for contacting us!'
         message = f'Hi {fullname},\n\nThank you for contacting us. We have received your inquiry and will get back to you shortly.'
         from_email = settings.EMAIL_HOST_USER
         recipient_list = [email]
 
+        # Send the email to the user
         send_mail(subject, message, from_email, recipient_list)
-
-        admin_email = settings.RECEIVER_EMAIL
+        print("mail sent 1")
         admin_subject = 'New Inquiry Received'
-        admin_message = f'New inquiry received from {fullname}. Email: {email}, Phone: {phone_number}'
-        send_mail(admin_subject, admin_message, from_email, [admin_email])
+        admin_message = (
+            f'New inquiry received from {fullname}.\n'
+            f'Email: {email}\n'
+            f'Phone: {phone_number}\n'
+            f'Car Type: {car_type}\n'
+            f'Car Year: {car_year}\n'
+            f'Car Make: {car_make}\n'
+            f'Car Model: {car_model}\n'
+            f'Running Condition: {car_run}\n'
+            f'Type of Carrier: {type_of_carrier}\n'
+            f'Origin: {origin_city}, {origin_state} {origin_zip}\n'
+            f'Destination: {destination_city}, {destination_state} {destination_zip}\n'
+            f'Proposed Ship Date: {proposed_ship_date}\n'
+            f'Comment: {comment}'
+        )
+
+        # Send the email to the admin
+        send_mail(admin_subject, admin_message, from_email, [
+            "support@weproautotransport.com",
+            "hi@moorfo.uz",
+            "leads@wpa.msgplane.com"
+        ])
+        print("mail sent")
 
         return redirect('contact')
